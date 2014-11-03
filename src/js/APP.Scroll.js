d51a781e601ee6f8102ae418b9c05176e1828e7c
Thulio Philipe <thulioph@gmail.com>
2014-11-03 20:42:40 -0300

    Adiciona animação em todas as páginas

--- src/js/APP.Scroll.js
+++ src/js/APP.Scroll.js
@@ -2,13 +2,17 @@ var APP = APP || {};
 APP.Scroll = {
   setUp: function() {
     this.startScroll();
+    APP.Timeline.setUp();
   },
 
   startScroll: function() {
-    window.sr = new scrollReveal({
+    var config = {
+      mobile: false,
       reset: true,
       move: '50px',
       mobile: true
-    });
+    }
+
+    window.sr = new scrollReveal(config);
   }
 }
